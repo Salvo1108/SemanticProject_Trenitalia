40248d67b68c04e3369ca38be7eddafe461ad08d
Salvatore <48434490+Salvo1108@users.noreply.github.com>
2022-06-17 12:12:46 +0200

    Update queries

--- queries.py
+++ queries.py
@@ -97,18 +97,18 @@ def query_condition_rdate_nan(path: str, nature: str, place: str):
     return l_elt
 
 
-def query_get_train_station_by_zipcode(path: str, zipcode: str):
+def query_get_train_station_by_CAP(path: str, CAP: str):
     graph = start_queries(path)
     condition_query1 = """
-        SELECT DISTINCT ?place ?zipcode
+        SELECT DISTINCT ?place ?CAP
         WHERE{
     """
     condition_query2 = f"""
             ?nature rdfs:subClassOf ns:Object .
             ?obj rdf:type ?nature .
             ?obj ns:hasBeenFoundHere ?place.
-            ?place ns:zipcode ?zipcode.
-            FILTER(?zipcode = "{zipcode}")
+            ?place ns:CAP ?CAP.
+            FILTER(?CAP = "{CAP}")
     """
     condition_query3 = """
     }
@@ -139,7 +139,7 @@ def query_get_last_date_of_lost_objects(path: str):
 def query_get_lat_long_name_train_station(path: str):
     graph = start_queries(path)
     condition_query = """
-        SELECT ?lat ?long ?name ?city ?zipcode ?dep
+        SELECT ?lat ?long ?name ?city ?CAP ?dep
         WHERE{
             ?nature rdfs:subClassOf ns:Object .
             ?obj rdf:type ?nature .
@@ -148,19 +148,19 @@ def query_get_lat_long_name_train_station(path: str):
             ?place ns:longitude ?long.
             ?place ns:name ?name.
             ?place ns:city ?city.
-            ?place ns:zipcode ?zipcode.
+            ?place ns:CAP ?CAP.
             ?place ns:department ?dep.
     }
     """
     res = graph.query(prefix + '\n' + condition_query)
-    l_elt = [{'lat': row.lat.value, 'lon': row.long.value, 'popup': f'{row.name.value} - {row.city.value} {row.zipcode.value} {row.dep.value}'} for row in res]
+    l_elt = [{'lat': row.lat.value, 'lon': row.long.value, 'popup': f'{row.name.value} - {row.city.value} {row.CAP.value} {row.dep.value}'} for row in res]
     return l_elt
 
 
-def query_get_lost_object_with_conditions(path: str, nature: str, zipcode: str, hasRecoveredDate: str):
+def query_get_lost_object_with_conditions(path: str, nature: str, CAP: str, hasRecoveredDate: str):
     graph = start_queries(path)
     condition_query1 = """
-    SELECT ?type ?fDate ?rDate ?zipcode ?lat ?long ?city ?name
+    SELECT ?type ?fDate ?rDate ?CAP ?lat ?long ?city ?name
     WHERE {
     """
     condition_query2 = f"""
@@ -175,17 +175,17 @@ def query_get_lost_object_with_conditions(path: str, nature: str, zipcode: str, 
         condition_query_optional = f"""
             FILTER(?rDate != "nan")
             ?obj ns:hasBeenFoundHere ?place.
-            ?place ns:zipcode ?zipcode.
+            ?place ns:CAP ?CAP.
         """
     else:
         condition_query_optional = f"""
             FILTER(?rDate = "nan")
             ?obj ns:hasBeenFoundHere ?place.
-            ?place ns:zipcode ?zipcode.
+            ?place ns:CAP ?CAP.
         """
 
-    if zipcode != 'zipcode':
-        condition_query_optional2 = f'    FILTER(?zipcode = "{zipcode}")\n'
+    if CAP != 'CAP':
+        condition_query_optional2 = f'    FILTER(?CAP = "{CAP}")\n'
     else:
         condition_query_optional2 = ''
 
@@ -200,17 +200,17 @@ def query_get_lost_object_with_conditions(path: str, nature: str, zipcode: str, 
     """
     res = graph.query(prefix + '\n' + condition_query1 + condition_query2 + condition_query_optional + condition_query_optional2 + condition_query3 + condition_query4)
     # print(prefix + '\n' + condition_query1 + condition_query2 + condition_query_optional + condition_query_optional2 + condition_query3 + condition_query4)
-    l_elt = [[nature, row.type.value, row.fDate.value, row.rDate.value, row.zipcode.value, row.lat.value, row.long.value, row.city.value] for row in res]
+    l_elt = [[nature, row.type.value, row.fDate.value, row.rDate.value, row.CAP.value, row.lat.value, row.long.value, row.city.value] for row in res]
     df = convert_list_queries_to_df(l_queries=l_elt,
                                     l_cols=['Nature of the Object', 'Type of the Object', 'Found Date', 'Recovered Date',
-                                            'Zipcode of the Station', 'Latitude', 'longitude', 'City of the Station'])
+                                            'CAP of the Station', 'Latitude', 'longitude', 'City of the Station'])
     return df
 
 
-def query_get_all_lost_object_with_conditions(path: str, zipcode: str, hasRecoveredDate: str):
+def query_get_all_lost_object_with_conditions(path: str, CAP: str, hasRecoveredDate: str):
     graph = start_queries(path)
     condition_query1 = """
-    SELECT ?nature ?type ?fDate ?rDate ?zipcode ?lat ?long ?city ?name
+    SELECT ?nature ?type ?fDate ?rDate ?CAP ?lat ?long ?city ?name
     WHERE {
     """
     condition_query2 = f"""
@@ -225,18 +225,18 @@ def query_get_all_lost_object_with_conditions(path: str, zipcode: str, hasRecove
         condition_query_optional = f"""
             FILTER(?rDate != "nan")
             ?obj ns:hasBeenFoundHere ?place.
-            ?place ns:zipcode ?zipcode.
+            ?place ns:CAP ?CAP.
         """
     else:
         condition_query_optional = f"""
             FILTER(?rDate = "nan")
             ?obj ns:hasBeenFoundHere ?place.
             ?place ns:name ?name.
-            ?place ns:zipcode ?zipcode.
+            ?place ns:CAP ?CAP.
         """
 
-    if zipcode != 'zipcode':
-        condition_query_optional2 = f'    FILTER(?zipcode = "{zipcode}")\n'
+    if CAP != 'CAP':
+        condition_query_optional2 = f'    FILTER(?CAP = "{CAP}")\n'
     else:
         condition_query_optional2 = ''
 
@@ -251,19 +251,19 @@ def query_get_all_lost_object_with_conditions(path: str, zipcode: str, hasRecove
     """
     res = graph.query(prefix + '\n' + condition_query1 + condition_query2 + condition_query_optional + condition_query_optional2 + condition_query3 + condition_query4)
     # print(prefix + '\n' + condition_query1 + condition_query2 + condition_query_optional + condition_query_optional2 + condition_query3 + condition_query4)
-    l_elt = [[row.nature.split('#')[1], row.type.value, row.fDate.value, row.rDate.value, row.zipcode.value, row.lat.value, row.long.value, row.city.value] for row in res]
+    l_elt = [[row.nature.split('#')[1], row.type.value, row.fDate.value, row.rDate.value, row.CAP.value, row.lat.value, row.long.value, row.city.value] for row in res]
     df = convert_list_queries_to_df(l_queries=l_elt,
                                     l_cols=['Nature of the Object', 'Type of the Object', 'Found Date', 'Recovered Date',
-                                            'Zipcode of the Station', 'Latitude', 'longitude', 'City of the Station'])
+                                            'CAP of the Station', 'Latitude', 'longitude', 'City of the Station'])
     return df
 
 
 if __name__ == "__main__":
     path_owl_file = './data/output_context.owl'
 
-    q = query_get_lost_object_with_conditions(path_owl_file, 'Carte_d_identite_passeport_permis_de_conduire', 'zipcode', 'Oui')
+    q = query_get_lost_object_with_conditions(path_owl_file, 'Carte_d_identite_passeport_permis_de_conduire', 'CAP', 'Oui')
     # q = query_get_lost_object_with_conditions(path_owl_file, 'Carte_d_identite_passeport_permis_de_conduire', '75015', 'Oui')
-    # q = query_get_lost_object_with_conditions(path_owl_file, 'All', 'zipcode', 'Oui')
+    # q = query_get_lost_object_with_conditions(path_owl_file, 'All', 'CAP', 'Oui')
     # q = query_get_lost_object_with_conditions(path_owl_file, 'All', '75015', 'Oui')
     # q = query_get_all_nature(path_owl_file)
     # q = query_get_last_date_of_lost_objects(path_owl_file)
